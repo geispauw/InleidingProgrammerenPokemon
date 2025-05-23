1a858bf9d6a2016970e754d9cb00da0209a351a7
Geis <82882147+geispauw@users.noreply.github.com>
2025-05-23 18:34:28 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 let HP = 30;
-let OppHP = 30;
+let OppHP = 60;
 let PlayerHP = document.getElementById("PlayerHP");
 let EnemyHP = document.getElementById("EnemyHP");
-let Attackbutton = document.getElementById("Attack");
+let PotionAmount = 5;
+let StrongPotionAmount = 3;
+const Attackbutton = document.getElementById("Attack");
 const AttackDiv = document.querySelector(".AttacksDiv");
 const MovesDiv = document.querySelector(".MovesDiv");
 const ItemsDiv = document.querySelector(".ItemsDiv")
@@ -16,113 +18,161 @@ const Item1 = document.getElementById("Item1");
 const Item2 = document.getElementById("Item2");
 const Item3 = document.getElementById("Terug");
 const Terug = document.getElementById("back");
+const CharizardDiv = document.querySelector(".CharizardAttackDiv");
+const TextboxDiv = document.querySelector(".Textbox");
+let Damage;
 
-//Zet een accuracy % voor de span van de game.
-const EnemyAccuracy = Math.floor(Math.random()*(100-37+1))+37;
-
-//3 enemy attacks met de accuracy en damage gerandomized
-function EnemyAttack1 () {
-    
-    if (EnemyAccuracy>=50) {
-        let Damage = Math.floor(Math.random()*5)+4; //Damage tussen 4 en 8
-        PlayerHP.value-=Damage;
-        console.log("Charizard heeft", PlayerHP.value, "HP")
+//Calculates Enemy Accuracy every turn, and bases its move off htat.
+function CalculateDamage() {
+    const EnemyAccuracy = Math.floor(Math.random()*(100-50+1))+50;
+    if (EnemyAccuracy >= 50) {
+        Damage = Math.floor(Math.random()*6)+3; //Damage tussen 2 en 8
+    } else if (EnemyAccuracy>= 65) {
+        Damage = Math.floor(Math.random()*5)+5; //Damage tussen 5 en 9
+    } else if (EnemyAccuracy >= 60) {
+        Damage = Math.floor(Math.random()*6)+2; //Damage tussen 2 en 6
     } else {
-        console.log("Pokemon heeft gemist")
+        Damage = 0;
+    }
+    console.log(Damage);
+    console.log(EnemyAccuracy);
+};
+
+function EnemyAttack() {
+    CalculateDamage();
+    if (Damage > 0) {
+        PlayerHP.value -= Damage;
+        TextboxDiv.textContent= "Charizard heeft " + Damage + " Damage genomen en heeft nu " + PlayerHP.value + " HP";
+    } else {
+        TextboxDiv.textContent= "Charizard heeft gemist";
     }
 };
 
-function EnemyAttack2 () {
-    
-    if (EnemyAccuracy>= 65) {
-        let Damage = Math.floor(Math.random()*10)+2; //Damage tussen 2 en 12
-        PlayerHP.value-=Damage;
-        console.log("Charizard heeft", PlayerHP.value, "HP")
-    } else {
-        console.log ("Pokemon heeft gemist")
-    }
-};
-
-function EnemyAttack3 () {
-    if (EnemyAccuracy >= 35) {
-        let Damage = Math.floor(Math.random()*3)+2; //Damage tussen 2 en 5
-        PlayerHP.value-=Damage;
-        console.log("Charizard heeft", PlayerHP.value, "HP")
-    } else {
-        console.log("Pokemon heeft gemist.")
-    }
-};
-
-//Enemy Attack Randomizer
-function RandomEnemyAttack () { 
-    let Dice = Math.floor(Math.random()*3)+1; // Dice van 3 kanten die de attack kiest.
-    console.log(Dice);
-    if (Dice==1) {
-        EnemyAttack1();
-        console.log("attack 1");
-    } else if (Dice==2) {
-        EnemyAttack2();
-        console.log("attack 2");
-    } else {
-        EnemyAttack3();
-        console.log("attack 3");
-    }
-};
-
-function PlayerAttack1 () {
-    let Damage = Math.floor(Math.random()*3)+2; //Damage tussen 2 en 5
+function PlayerAttack1() {
+    let Damage = Math.floor(Math.random()*3)+4; //Damage tussen 4 en 7
     EnemyHP.value-= Damage;
     OppHP-= Damage;
     console.log("attack1");    
 };
 
-function PlayerAttack2 () {
-    let Damage = Math.floor(Math.random()*3)+2; //Damage tussen 2 en 5
+function PlayerAttack2() {
+    let Damage = Math.floor(Math.random()*3)+3; //Damage tussen 3 en 6
     EnemyHP.value-= Damage;
     OppHP-= Damage;
     console.log("attack2");
 };
 
-function PlayerAttack3 () {
-    let Damage = Math.floor(Math.random()*3)+2; //Damage tussen 2 en 5
+function PlayerAttack3() {
+    let Damage = Math.floor(Math.random()*4)+3; //Damage tussen 3 en 7
     EnemyHP.value -= Damage;
     OppHP -= Damage;
     console.log("attack3");
 };
-
-function Healing () {
-    PlayerHP.value += 5;
+//Checkt of de player nog potions heeft en healt de player.
+function Healing() {
+    if (PotionAmount > 0) {
+        PlayerHP.value += 5;
+        PotionAmount -= 1;
+        ItemsDiv.classList.add("hidden");
+        TextboxDiv.classList.remove("hidden");
+        TextboxDiv.textContent = "Je hebt nog " + PotionAmount + " Aantal Potions!";
+        setTimeout(TextBackToMoves, 1000);
+    } else if (PotionAmount = 0) {
+        ItemsDiv.classList.add("hidden");
+        TextboxDiv.classList.remove("hidden");
+        TextboxDiv.textContent = "Je hebt geen potions meer!";
+        setTimeout(TextBackToMoves, 1000);
+    }
 };
 
-function StrongHealing () {
-    PlayerHP.value += 10;
+function StrongHealing() {
+    if (StrongPotionAmount > 0) {
+        PlayerHP.value += 10;
+        StrongPotionAmount -= 1;
+        ItemsDiv.classList.add("hidden");
+        TextboxDiv.classList.remove("hidden");
+        TextboxDiv.textContent = "Je hebt nog " + StrongPotionAmount + " Aantal Strong Potions!";
+        setTimeout(TextBackToMoves, 1000);
+    } else if (StrongPotionAmount = 0) {
+        ItemsDiv.classList.add("hidden");
+        TextboxDiv.classList.remove("hidden");
+        TextboxDiv.textContent = "Je hebt geen potions meer!";
+        setTimeout(TextBackToMoves, 1000);
+    }
 };
 
 //Change Moves Div to Attack Div
-function ChangetoAttack (){
+function ChangetoAttack(){
     AttackDiv.classList.remove("hidden");
     MovesDiv.classList.add("hidden");
     
 };
 
 //Change Moves Div to Items Div
-function ChangetoItems (){
+function ChangetoItems(){
     ItemsDiv.classList.remove("hidden");
     MovesDiv.classList.add("hidden");
     
 };
 
-//Attack back to Move 
-function BacktoMoves () {
+//Attack div back to Move div
+function BacktoMoves() {
     AttackDiv.classList.add("hidden");
     MovesDiv.classList.remove("hidden");
+    CharizardDiv.classList.add("hidden");
 };
 
-//Ites back to move
-function ItemsBacktoMoves () {
+function TextBackToMoves() {
+    TextboxDiv.classList.add("hidden");
+};
+
+//Items div back to move div and changes to Charizard Prep div
+function ItemsBacktoMoves() {
     ItemsDiv.classList.add("hidden");
     MovesDiv.classList.remove("hidden");
+    CharizardDiv.classList.add("hidden");
 };
+
+//Set prep div to "charizard is attacking"
+function CharizardAttackDiv() {
+    CharizardDiv.classList.remove("hidden");
+    AttackDiv.classList.add("hidden");
+    ItemsDiv.classList.add("hidden");
+};
+
+//Set div to "Charizard missed" or "Charizard did x amounts of damage"
+function CharizardDmgCheck() {
+    TextboxDiv.classList.remove("hidden");
+    CharizardDiv.classList.add("hidden");
+};
+
+//Checks who won, first it checks if the player did enough damage to finish the enemy.
+//if they didnt, it continues and after the enemy attacks, it checks if they did enough damage to finish the player.
+function PostMoveCheck() {
+    if (EnemyHP.value <= 0) {
+        TextboxDiv.classList.remove("hidden");
+        TextboxDiv.textContent= "Je hebt Gewonnen!";
+        AttackDiv.classList.add("hidden");
+    } else {
+        CharizardAttackDiv();
+        setTimeout(CharizardDmgCheck, 1000);
+        setTimeout(EnemyAttack, 1000);
+        setTimeout(PlayerHPCheck, 1001);
+    };
+};
+
+//Checks if the enemy did enough damage to finish the player.
+function PlayerHPCheck () {
+    if (PlayerHP.value <= 0) {
+            TextboxDiv.textContent= "Je hebt verloren...";
+            ItemsDiv.classList.add("hidden");
+            AttackDiv.classList.add("hidden");
+    } else {
+        setTimeout(BacktoMoves, 2000);
+        setTimeout(TextBackToMoves, 2000);
+        }
+};
+
 
 Attack.addEventListener("click", ChangetoAttack);
 
@@ -130,58 +180,31 @@ Item.addEventListener("click", ChangetoItems);
 
 Item1.addEventListener("click", () => {
     Healing();
-    setTimeout(RandomEnemyAttack, 2000);
-    setTimeout(ItemsBacktoMoves, 2000);
-    setTimeout(checkWinOrLoss, 2000);
+    setTimeout(PostMoveCheck, 1000);
 });
 
 Item2.addEventListener("click", () => {
     StrongHealing();
-    setTimeout(RandomEnemyAttack, 2000);
-    setTimeout(ItemsBacktoMoves, 2000);
-    setTimeout(checkWinOrLoss, 2000);
+    setTimeout(PostMoveCheck, 1000);
 });
 
-Item3.addEventListener("click", () => {
-    ItemsBacktoMoves();
-});
+Item3.addEventListener("click", ItemsBacktoMoves);
 
 Attack1.addEventListener("click", () => {
     PlayerAttack1();
-    setTimeout(RandomEnemyAttack, 2000);
-    setTimeout(BacktoMoves, 2000);
-    setTimeout(checkWinOrLoss, 2000);
+    PostMoveCheck();
 });
 
 Attack2.addEventListener("click", () => {
     PlayerAttack2();
-    setTimeout(RandomEnemyAttack, 2000);
-    setTimeout(BacktoMoves, 2000);
-    setTimeout(checkWinOrLoss, 2000);
+    PostMoveCheck();
 });
 
 Attack3.addEventListener("click", () => {
     PlayerAttack3();
-    setTimeout(RandomEnemyAttack, 2000);
-    setTimeout(BacktoMoves, 2000);
-    setTimeout(checkWinOrLoss, 2000);
+    PostMoveCheck();
 
 });
 
-Terug.addEventListener("click", () => {
-    BacktoMoves();
-});
-
-
-function checkWinOrLoss() {
-    if (PlayerHP.value<=0) {
-        console.log("You Lost...");
-    } else if (EnemyHP.value<=0) {
-        console.log("You Win!");
-    }
-}
-
-function Onehit(){
-    EnemyHP.value-=30;
-}
+Terug.addEventListener("click", BacktoMoves);
 
